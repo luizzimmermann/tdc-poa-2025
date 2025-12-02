e22d4786bb642aec3517c6b6a43720e60e4ff75d
Marcus Hert da Coregio <marcusdacoregio@gmail.com>
2025-12-02 18:45:58 -0300

    Add keycloak-angular sample

---
+++ keycloak-angular/projects/examples/ngmodule/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+import { KeycloakProfile } from 'keycloak-js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false
+})
+export class AppComponent implements OnInit {
+  private readonly keycloak = inject(KeycloakService);
+
+  public isLoggedIn = false;
+  public userProfile: KeycloakProfile | null = null;
+
+  public async ngOnInit() {
+    this.isLoggedIn = await this.keycloak.isLoggedIn();
+
+    if (this.isLoggedIn) {
+      this.userProfile = await this.keycloak.loadUserProfile();
+    }
+  }
+
+  public login() {
+    this.keycloak.login();
+  }
+
+  public logout() {
+    this.keycloak.logout();
+  }
+}
